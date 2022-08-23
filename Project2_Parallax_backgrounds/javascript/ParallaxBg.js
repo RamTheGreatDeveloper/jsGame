05c8e8843d29737991f555903b79031123ca2d65
RamTheGreatDeveloper <ramkumars.trichy@gmail.com>
2022-08-23 07:51:10 +0530

    add : new parallax bg method

--- Project2_Parallax_backgrounds/javascript/ParallaxBg.js
+++ Project2_Parallax_backgrounds/javascript/ParallaxBg.js
@@ -4,7 +4,7 @@ const canvas_height = canvas.height = 700;
 const canvas_width = canvas.width = 800;
 
 let gameSpeed = 15;
-
+let gameFrame =5;
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'assets/layer-1.png';
 const backgroundLayer2 = new Image();
@@ -22,22 +22,13 @@ class Layer{
         this.y =0;
         this.width = 2400;
         this.height = 700;
-        // this.x2 = this.width;
         this.image = image;
         this.speedModifier = speedModifier;
         this.speed = gameSpeed * this.speedModifier;
     }
     update(){
         this.speed = gameSpeed * this.speedModifier;
-        if(this.x <= -this.width){
-            // this.x = this.width + this.x2 - this.speed;
-           this.x= 0
-        }
-        // if(this.x2 <= -this.width){
-        //     this.x2 = this.width + this.x - this.speed;
-        // }
-        this.x = Math.floor(this.x - this.speed);
-        // this.x2 = Math.floor(this.x2 - this.speed);
+        this.x = gameFrame * this.speed % this.width;
     }
     draw(){
         ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
@@ -67,7 +58,7 @@ function animate(){
         Object.update();
         Object.draw();
     })
-
+    gameFrame--;
     requestAnimationFrame(animate);
 }
 animate();
