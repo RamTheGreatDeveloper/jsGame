58d33195de13d621e0ab6fe93783d394533138e1
RamTheGreatDeveloper <ramkumars.trichy@gmail.com>
2022-08-24 02:42:00 +0530

    add : animate function create

--- Project3_Enemy_movement_patterns/javascript/enemyMov.js
+++ Project3_Enemy_movement_patterns/javascript/enemyMov.js
@@ -1,5 +1,21 @@
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
-const canvas_width = canvas.width = 330;
-const canvas_height = canvas.height = 333;
+let canvas_width = canvas.width = 500;
+let canvas_height = canvas.height = 1000;
+
+let enemy1 = {
+    x:0,
+    y:0,
+    width:200,
+    height:200,
+}
+
+function animate(){
+    ctx.clearRect(0,0,canvas_width,canvas_height);
+    ctx.fillRect(enemy1.x,enemy1.y,enemy1.width,enemy1.height);
+    enemy1.x++;
+    requestAnimationFrame(animate);
+}
+animate();
