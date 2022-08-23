7a135810a314935215497b046925210933a0292a
RamTheGreatDeveloper <ramkumars.trichy@gmail.com>
2022-08-23 07:42:25 +0530

    add : slider added

--- Project2_Parallax_backgrounds/javascript/ParallaxBg.js
+++ Project2_Parallax_backgrounds/javascript/ParallaxBg.js
@@ -43,11 +43,30 @@ class Layer{
         ctx.drawImage(this.image,this.x2,this.y,this.width,this.height);
     }
 }
-const layer4 = new Layer(backgroundLayer4,0.5);
+const layer1 = new Layer(backgroundLayer1,0.2);
+const layer2 = new Layer(backgroundLayer2,0.4);
+const layer3 = new Layer(backgroundLayer3,0.6);
+const layer4 = new Layer(backgroundLayer4,0.8);
+const layer5 = new Layer(backgroundLayer5,1);
+
+const slider = document.getElementById('slider');
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById('showGameSpeed');
+showGameSpeed.innerHTML = gameSpeed;
+
+slider.addEventListener('change',function(e){
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
+})
+
+const gameLayers = [layer1,layer2,layer3,layer4,layer5];
 function animate(){
     ctx.clearRect(0,0,canvas_width,canvas_height);
-    layer4.update();
-    layer4.draw();
+    gameLayers.forEach(Object =>{
+        Object.update();
+        Object.draw();
+    })
+
     requestAnimationFrame(animate);
 }
 animate();
